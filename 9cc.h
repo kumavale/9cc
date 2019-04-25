efa6e0ea35ae6cb708ee175edac304face6231e6
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-04-25 11:52:09 +0900

    Add _strndup() and Preparation of file division

--- 9cc.h
+++ 9cc.h
@@ -1,3 +1,8 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
 // トークンの型を表す値
 enum {
--- test.sh
+++ test.sh
@@ -17,22 +17,23 @@ try() {
     fi
 }
 
-try 42 "42;"
-try 21 '5+20-4;'
-try 41 " 12 + 34 - 5 ;"
-try 47 "5+6*7;"
-try 15 "5*(9-6);"
-try 4 "(3+5)/2;"
-try 2 "1+2*(3-4);(4+2)/3;"
-try 3 "a=1; a+a+a;"
-try 6 "a=1; b=2; c=3; a*b*c;"
-try 14 "a=3; b=5*6-8; a+b/2;"
-try 0 "return 0;"
-try 42 "r=42; return r;"
-try 4 "a=b=2; a*b;"
-try 6 "foo = 1; bar = 2 + 3; return foo + bar;"
-try 13 "hoge=5; fuga=hoge*2; fuga+3;"
-try 6 "i=-5; k=j=-(2+i); j+k;"
+try 42  "42;"
+try 21  "5+20-4;"
+try 41  " 12 + 34 - 5 ;"
+try 47  "5+6*7;"
+try 15  "5*(9-6);"
+try 4   "(3+5)/2;"
+try 2   "1+2*(3-4);(4+2)/3;"
+try 3   "a=1; a+a+a;"
+try 6   "a=1; b=2; c=3; a*b*c;"
+try 14  "a=3; b=5*6-8; a+b/2;"
+try 0   "return 0;"
+try 42  "r=42; return r;"
+try 4   "a=b=2; a*b;"
+try 6   "foo = 1; bar = 2 + 3; return foo + bar;" #unfinished
+try 13  "hoge=5; fuga=hoge*2; fuga+3;" #unfinished
+#try 6   "a=1; ab=2; abc=3; return a+ab+abc;"
+try 6   "i=-5; k=j=-(2+i); j+k;"
 #try -15 "-3*+5;" #failured
 
 echo OK
