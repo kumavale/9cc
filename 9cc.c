0afb730d7fbef91d6c478977d16dcbb470c40790
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-09-21 13:08:38 +0900

    :sparkles:Add AST

--- 9cc.c
+++ 9cc.c
@@ -21,6 +21,23 @@ struct Token {
     char *str;       // Token string
 };
 
+// Kind of AST
+typedef enum {
+    ND_ADD,  // +
+    ND_SUB,  // -
+    ND_MUL,  // *
+    ND_DIV,  // /
+    ND_NUM,  // Integer
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // Node type
+    Node *lhs;      // Left hand side
+    Node *rhs;      // Right hand side
+    int val;        // Only use when kind is ND_NUM
+};
+
 // Current token
 Token *token;
 
@@ -50,6 +67,21 @@ void error_at(char *loc, char *fmt, ...) {
     exit(1);
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs  = lhs;
+    node->rhs  = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val  = val;
+    return node;
+}
+
 // Consumes the current token if it matches 'op'.
 bool consume(char op) {
     if (token->kind != TK_RESERVED || token->str[0] != op) {
@@ -75,6 +107,77 @@ int expect_number() {
     int val = token->val;
     token = token->next;
     return val;
+}
+
+Node *expr();
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *expr() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
 }
 
 bool at_eof() {
